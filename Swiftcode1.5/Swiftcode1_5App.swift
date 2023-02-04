066d5905e1476766d495be6e26a9adb2121383f3
Astrid Snäll <vaski2astrid@hotmail.com>
2023-02-04 15:11:59 -0500

    Initial Commit

---
+++ Swiftcode1.5/Swiftcode1_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftcode1_5App.swift
+//  Swiftcode1.5
+//
+//  Created by Astrid Snäll on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Swiftcode1_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
