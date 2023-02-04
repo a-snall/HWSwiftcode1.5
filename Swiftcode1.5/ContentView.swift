db88a38504217452130c4da65deb7d11efb9f0ef
Astrid Snäll <vaski2astrid@hotmail.com>
2023-02-04 15:32:55 -0500

    HW 1.5

--- Swiftcode1.5/ContentView.swift
+++ Swiftcode1.5/ContentView.swift
@@ -8,19 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var messageString = " wu "
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+         
+        VStack  {
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .border(.orange, width: 1)
+                .padding()
+                
+            
+            HStack {
+                Button("Awesome") {
+                    messageString = "You are awesome"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Awesome") {
+                    messageString = "You are awesome"
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .border(.purple, width: 5)
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
